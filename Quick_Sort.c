68c4c8cfab092b672dcbd2bb581d0d0b0e56ab49
narmathasree710-coder <narmathasree710@gmail.com>
2026-06-03 09:58:26 +0530

    Create Quick_Sort.c

---
+++ Quick_Sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void quicksort(int a[], int low, int high)
+{
+    int i = low, j = high;
+    int pivot = a[(low + high) / 2];
+    int temp;
+    while(i <= j)
+    {
+        while(a[i] < pivot)
+            i++;
+        while(a[j] > pivot)
+            j--;
+        if(i <= j)
+        {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    if(low < j)
+        quicksort(a, low, j);
+    if(i < high)
+        quicksort(a, i, high);
+}
+int main()
+{
+    int n, i, a[20];
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    quicksort(a, 0, n - 1);
+    printf("Sorted array: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
